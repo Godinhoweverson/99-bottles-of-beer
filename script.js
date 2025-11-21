12c5b5c92146f355d54b984ebdd8446e1ff30ce2
Godinhoweverson <weverson.godinho@icloud.com>
2025-11-21 10:50:29 +0000

    Fix some type errors

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ while(bottles >= 0){
     if(bottles < 1){
         li.textContent = "No more bottles of beer on the wall, no more bottles of beer Go to the store and buy some more, 99 bottles of beer on the wall"
     }else if(bottles === 1){
-        li.textContent = `${bottles} bottle of beer on the wall, ${bottles} bottle of beer \n
+        li.textContent = `${bottles} bottle of beer on the wall, ${bottles} bottle of beer.
                             Take one down and pass it around, no more bottles of beer on the wall`
     }else{
-        li.textContent = `${bottles} bottle${bottles > 1 ? "s" : " "} of beer on the wall, ${bottles} bottle${bottles > 1 ? "s" : " "} of beer \n
+        li.textContent = `${bottles} bottle${bottles > 1 ? "s" : " "} of beer on the wall, ${bottles} bottle${bottles > 1 ? "s" : " "} of beer.
                             Take one down and pass it around, ${bottles - 1} bottle${(bottles - 1) > 1 ? "s" : " "} of beer on the wall`
     }
   
